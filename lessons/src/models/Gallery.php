bf10d27404c02298879c82ab7f4f34ecdd083042
robotvenomchik <96344010+robotvenomchik@users.noreply.github.com>
2023-10-27 19:20:55 +0300

    router3.0

--- lessons/src/models/Gallery.php
+++ lessons/src/models/Gallery.php
@@ -4,5 +4,28 @@ namespace src\models;
 
 class Gallery
 {
+    public function findAll():array
+    {
+        return [
+            0 => [
+                'first_name' => 'test1',
+                'second_name' => 'test2',
+                'login' => 'test3',
+                'email' => 'test@test.com',
+            ],
+            1 => [
+                'first_name' => 'test1',
+                'second_name' => 'test2',
+                'login' => 'test3',
+                'email' => 'test4@test.com',
+            ],
+            2 => [
+                'first_name' => 'test1',
+                'second_name' => 'test2',
+                'login' => 'test3',
+                'email' => 'test2@test.com',
+            ],
 
+        ];
+    }
 }
